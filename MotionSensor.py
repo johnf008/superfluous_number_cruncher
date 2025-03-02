8ef7acaa9b7c96b25f99a883aeb087d5a1e4c8ce
Rockets Space <115192113+RocketsSpace@users.noreply.github.com>
2025-03-02 09:37:38 -0500

    funky

--- MotionSensor.py
+++ MotionSensor.py
@@ -1,10 +1,14 @@
 import RPi.GPIO as GPIO
 import time
+import threading
 from gpiozero import MotionSensor
 from signal import pause 
 
-pir = MotionSensor(18)  #I think this defines the sensor
+pir = MotionSensor(12)  #I think this defines the sensor
 #figure out how to 
+
+
+
 def motion_function():
     print("Motion Detected")  #this will most likely set the screen to true
 
@@ -13,6 +17,9 @@ def no_motion_function():
 
 pir.when_motion = motion_function
 pir.when_no_motion = no_motion_function
-
+while True:
+	pir.wait_for_motion()
+	print("You moved")
+	pir.wait_for_no_motion()
 
 pause()
