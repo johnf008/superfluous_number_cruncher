5db15edf0e7472c52d3ab0f2b401e185a45de5c3
johnf008 <Carlos.flores.h108@gmail.com>
2025-03-02 13:01:47 -0600

    Update gui.py

--- gui.py
+++ gui.py
@@ -36,102 +36,6 @@ window.geometry("1024x600")
 window.configure(bg = "#FFFFFF")
 
 
-canvas = Canvas(
-    window,
-    bg = "#FFFFFF",
-    height = 600,
-    width = 1024,
-    bd = 0,
-    highlightthickness = 0,
-    relief = "ridge"
-)
-
-canvas.place(x = 0, y = 0)
-image_image_1 = PhotoImage(
-    file=relative_to_assets("image_1.png"))
-image_1 = canvas.create_image(
-    512.0,
-    300.0,
-    image=image_image_1
-)
-
-image_image_2 = PhotoImage(
-    file=relative_to_assets("image_2.png"))
-image_2 = canvas.create_image(
-    517.0,
-    224.0,
-    image=image_image_2
-)
-
-image_image_3 = PhotoImage(
-    file=relative_to_assets("image_3.png"))
-image_3 = canvas.create_image(
-    516.0,
-    110.0,
-    image=image_image_3
-)
-
-entry_image_1 = PhotoImage(
-    file=relative_to_assets("entry_1.png"))
-entry_bg_1 = canvas.create_image(
-    160.0,
-    431.0,
-    image=entry_image_1
-)
-entry_1 = Text(
-    bd=0,
-    bg="#FFFFFF",
-    fg="#000716",
-    highlightthickness=0
-)
-entry_1.place(
-    x=71.0,
-    y=332.0,
-    width=178.0,
-    height=196.0
-)
-
-entry_image_2 = PhotoImage(
-    file=relative_to_assets("entry_2.png"))
-entry_bg_2 = canvas.create_image(
-    526.0,
-    431.0,
-    image=entry_image_2
-)
-entry_2 = Text(
-    bd=0,
-    bg="#FFFFFF",
-    fg="#000716",
-    highlightthickness=0
-)
-entry_2.place(
-    x=437.0,
-    y=332.0,
-    width=178.0,
-    height=196.0
-)
-
-entry_image_3 = PhotoImage(
-    file=relative_to_assets("entry_3.png"))
-entry_bg_3 = canvas.create_image(
-    892.0,
-    431.0,
-    image=entry_image_3
-)
-entry_3 = Text(
-    bd=0,
-    bg="#FFFFFF",
-    fg="#000716",
-    highlightthickness=0
-)
-entry_3.place(
-    x=803.0,
-    y=332.0,
-    width=178.0,
-    height=196.0
-)
-
-
 #### DHT11
 
 #hi
@@ -258,13 +162,108 @@ thread2Run.start()
 
 #### DHT11 end
 
+canvas = Canvas(
+    window,
+    bg = "#FFFFFF",
+    height = 600,
+    width = 1024,
+    bd = 0,
+    highlightthickness = 0,
+    relief = "ridge"
+)
+
+canvas.place(x = 0, y = 0)
+image_image_1 = PhotoImage(
+    file=relative_to_assets("image_1.png"))
+image_1 = canvas.create_image(
+    512.0,
+    300.0,
+    image=image_image_1
+)
+
+image_image_2 = PhotoImage(
+    file=relative_to_assets("image_2.png"))
+image_2 = canvas.create_image(
+    517.0,
+    224.0,
+    image=image_image_2
+)
+
+image_image_3 = PhotoImage(
+    file=relative_to_assets("image_3.png"))
+image_3 = canvas.create_image(
+    516.0,
+    110.0,
+    image=image_image_3
+)
+
+entry_image_1 = PhotoImage(
+    file=relative_to_assets("entry_1.png"))
+entry_bg_1 = canvas.create_image(
+    160.0,
+    431.0,
+    image=entry_image_1
+)
+entry_1 = Text(
+    bd=0,
+    bg="#FFFFFF",
+    fg="#000716",
+    highlightthickness=0
+)
+entry_1.place(
+    x=71.0,
+    y=332.0,
+    width=178.0,
+    height=196.0
+)
+
+entry_image_2 = PhotoImage(
+    file=relative_to_assets("entry_2.png"))
+entry_bg_2 = canvas.create_image(
+    526.0,
+    431.0,
+    image=entry_image_2
+)
+entry_2 = Text(
+    bd=0,
+    bg="#FFFFFF",
+    fg="#000716",
+    highlightthickness=0
+)
+entry_2.place(
+    x=437.0,
+    y=332.0,
+    width=178.0,
+    height=196.0
+)
+
+entry_image_3 = PhotoImage(
+    file=relative_to_assets("entry_3.png"))
+entry_bg_3 = canvas.create_image(
+    892.0,
+    431.0,
+    image=entry_image_3
+)
+entry_3 = Text(
+    bd=0,
+    bg="#FFFFFF",
+    fg="#000716",
+    highlightthickness=0
+)
+entry_3.place(
+    x=803.0,
+    y=332.0,
+    width=178.0,
+    height=196.0
+)
+
 button_image_1 = PhotoImage(
     file=relative_to_assets("button_1.png"))
 button_1 = Button(
     image=button_image_1,
     borderwidth=0,
     highlightthickness=0,
-    command=dht11.read_data,
+    command=lambda: print("button_1 clicked"),
     relief="flat"
 )
 button_1.place(
@@ -290,5 +289,4 @@ image_5 = canvas.create_image(
     image=image_image_5
 )
 window.resizable(False, False)
-
 window.mainloop()
